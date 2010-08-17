503b9e83c659ee4c68ba1554a2e3175ca10550b1
nsf <no.smile.face@gmail.com>
2010-08-17 16:49:37 +0600

    Add testing framework.
    Currently there are only 9 tests and they don't cover much of the
    funcionality. I'll try to add at least 3 tests per day.

---
+++ testing/run.all.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import os, glob, subprocess
+
+total = 0
+ok = 0
+fail = 0
+
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+NC = "\033[0m"
+
+OK = GREEN + "PASS!" + NC
+FAIL = RED + "FAIL!" + NC
+
+for t in sorted(glob.glob("test.*")):
+	total += 1
+	c = glob.glob(t + "/cursor.*")[0]
+	cursorpos = os.path.splitext(c)[1][1:]
+	with open(t + "/out.expected") as f:
+		outexpected = f.read()
+	filename = t + "/test.go"
+	gocode = subprocess.Popen("gocode -in %s autocomplete %s %s" % (filename, filename, cursorpos),
+			shell=True, stdout=subprocess.PIPE)
+	out = gocode.communicate()[0]
+	if out != outexpected:
+		print t + ": " + FAIL
+		print "--------------------------------------------------------"
+		print "Got:\n" + out
+		print "--------------------------------------------------------"
+		print "Expected:\n" + outexpected
+		print "--------------------------------------------------------"
+		fail += 1
+	else:
+		print t + ": " + OK
+		ok += 1
+
+print "\nSummary (total: %d):" % total
+print GREEN + "  PASS" + NC + ": %d" % ok
+print RED +"  FAIL" + NC + ": %d" % fail
+
+if fail == 0:
+	print GREEN + "████████████████████████████████████████████████████████████████████" + NC
+else:
+	print RED + "████████████████████████████████████████████████████████████████████" + NC
