4db54f2756c5f91ff88778387aca9d783136d5bd
nsf <no.smile.face@gmail.com>
2010-08-18 00:00:56 +0600

    Fix shell scripts.

--- test/test.bash
+++ test/test.bash
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 GOMAXPROCS=2 ./gocodetest $(find ${GOROOT}/pkg/${GOOS}_${GOARCH}/ -name "*.a" | xargs)
--- vim/update.bash
+++ vim/update.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 mkdir -p ~/.vim/{autoload,ftplugin}
 cp autoload/gocomplete.vim ~/.vim/autoload
 cp ftplugin/go.vim ~/.vim/ftplugin
