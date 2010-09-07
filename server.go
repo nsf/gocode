4a682ecc1d4dffeaf0604efa38bde7809ceb45d3
nsf <no.smile.face@gmail.com>
2010-09-07 18:57:52 +0600

    Add empty line after stack trace dump.

--- server.go
+++ server.go
@@ -56,6 +56,7 @@ func printBacktrace(err interface{}) {
 		fmt.Printf("%d(%s): %s:%d\n", i-1, f.Name(), file, line)
 		i++
 	}
+	fmt.Println("")
 }
 
 func Server_AutoComplete(file []byte, filename string, cursor int) (a, b, c []string, d int) {
