c0af691d5b7cea212d09e0be176d04896296dc80
Steve Purcell <steve@sanityinc.com>
2013-03-28 09:38:27 +0000

    Add Package-Requires header for ELPA installations
    This commit adds a header which tells package.el that
    go-autocomplete depends on auto-complete.

--- emacs/go-autocomplete.el
+++ emacs/go-autocomplete.el
@@ -4,6 +4,7 @@
 
 ;; Author: Mikhail <tensai@cirno.in> Kuryshev
 ;; Keywords: languages
+;; Package-Requires: ((auto-complete "1.4.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
