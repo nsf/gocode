16be5fd91f62b3990668671d2c3102e3b1690d2d
Lawrence Kesteloot <lk@teamten.com>
2013-08-20 22:12:02 -0700

    Don't overwrite user's go.vim ftplugin.
    The user may have a go.vim ftplugin for vim, so instead we make
    a "go" subdirectory and copy a file into it. (Vim runs every
    script in the filetype subdirectory.) The new filename matches
    the one we copy to autoload.

--- vim/update.sh
+++ vim/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p "$HOME/.vim/autoload"
-mkdir -p "$HOME/.vim/ftplugin"
+mkdir -p "$HOME/.vim/ftplugin/go"
 cp "${0%/*}/autoload/gocomplete.vim" "$HOME/.vim/autoload"
-cp "${0%/*}/ftplugin/go.vim" "$HOME/.vim/ftplugin"
+cp "${0%/*}/ftplugin/go/gocomplete.vim" "$HOME/.vim/ftplugin/go"
