8bf077615ce4fc75030daed6d95503f8750aa03f
nsf <no.smile.face@gmail.com>
2014-10-13 05:42:55 +0600

    Remove filtering noise from autocompletion results. Fixes #248.

--- decl.go
+++ decl.go
@@ -280,6 +280,9 @@ func check_for_anon_type(t ast.Expr, flags decl_flags, s *scope) ast.Expr {
 //-------------------------------------------------------------------------
 
 func new_decl_full(name string, class decl_class, flags decl_flags, typ, v ast.Expr, vi int, s *scope) *decl {
+	if name == "_" {
+		return nil
+	}
 	d := new(decl)
 	d.name = name
 	d.class = class
