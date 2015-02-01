b16f5e0efd132a088f83615a6a19c0a8b5a5f8ec
nsf <no.smile.face@gmail.com>
2015-02-01 08:33:46 +0500

    When running the server, use os.DevNull for file descriptors.
    Fixes #270.

--- client.go
+++ client.go
@@ -61,12 +61,27 @@ func try_run_server() error {
 	path := get_executable_filename()
 	args := []string{os.Args[0], "-s", "-sock", *g_sock, "-addr", *g_addr}
 	cwd, _ := os.Getwd()
-	procattr := os.ProcAttr{Dir: cwd, Env: os.Environ(), Files: []*os.File{nil, nil, nil}}
-	p, err := os.StartProcess(path, args, &procattr)
 
+	var err error
+	stdin, err := os.Open(os.DevNull)
 	if err != nil {
 		return err
 	}
+	stdout, err := os.OpenFile(os.DevNull, os.O_WRONLY, 0)
+	if err != nil {
+		return err
+	}
+	stderr, err := os.OpenFile(os.DevNull, os.O_WRONLY, 0)
+	if err != nil {
+		return err
+	}
+
+	procattr := os.ProcAttr{Dir: cwd, Env: os.Environ(), Files: []*os.File{stdin, stdout, stderr}}
+	p, err := os.StartProcess(path, args, &procattr)
+	if err != nil {
+		return err
+	}
+
 	return p.Release()
 }
 
