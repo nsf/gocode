2b99fc4d372b017483b7596c4577bf5f15479772
Nico Tonozzi <Nico Tonozzi>
2015-12-11 11:22:35 -0700

    Revert "Register company-go with company-backends"
    This reverts commit 6f18981971171520cc9ea522837916987187508f.
    
    This change caused issues with some company setups. Users should
    configure the backend in init scripts.

--- emacs-company/company-go.el
+++ emacs-company/company-go.el
@@ -203,6 +203,5 @@ triggers a completion immediately."
        (company-go--insert-arguments
         (get-text-property 0 'meta arg))))))
 
-(add-to-list 'company-backends 'company-go)
 (provide 'company-go)
 ;;; company-go.el ends here
