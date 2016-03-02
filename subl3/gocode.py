b3c1a9e603cc8651ed41de3c3d3910fa33f92b8f
nsf <no.smile.face@gmail.com>
2016-03-02 14:50:03 +0500

    Display returns as a hint and use args to form snippet directives.
    If candidate is a function parse its arguments and return values and use them
    for better completion suggestion display.
    
    Show return values in a hint section.
    
    Use arguments to form a snippet. Sublime can't display function signature
    nicely, so that is a workaround for this problem.

--- subl3/gocode.py
+++ subl3/gocode.py
@@ -1,5 +1,91 @@
 import sublime, sublime_plugin, subprocess
 
+# go to balanced pair, e.g.:
+# ((abc(def)))
+# ^
+# \--------->^
+#
+# returns -1 on failure
+def skip_to_balanced_pair(str, i, open, close):
+	count = 1
+	i += 1
+	while i < len(str):
+		if str[i] == open:
+			count += 1
+		elif str[i] == close:
+			count -= 1
+
+		if count == 0:
+			break
+		i += 1
+	if i >= len(str):
+		return -1
+	return i
+
+# split balanced parens string using comma as separator
+# e.g.: "ab, (1, 2), cd" -> ["ab", "(1, 2)", "cd"]
+# filters out empty strings
+def split_balanced(s):
+	out = []
+	i = 0
+	beg = 0
+	while i < len(s):
+		if s[i] == ',':
+			out.append(s[beg:i].strip())
+			beg = i+1
+			i += 1
+		elif s[i] == '(':
+			i = skip_to_balanced_pair(s, i, "(", ")")
+			if i == -1:
+				i = len(s)
+		else:
+			i += 1
+
+	out.append(s[beg:i].strip())
+	return list(filter(bool, out))
+
+
+def extract_arguments_and_returns(sig):
+	sig = sig.strip()
+	if not sig.startswith("func"):
+		return [], []
+
+	# find first pair of parens, these are arguments
+	beg = sig.find("(")
+	if beg == -1:
+		return [], []
+	end = skip_to_balanced_pair(sig, beg, "(", ")")
+	if end == -1:
+		return [], []
+	args = split_balanced(sig[beg+1:end])
+
+	# find the rest of the string, these are returns
+	sig = sig[end+1:].strip()
+	if sig.startswith("("):
+		sig = sig[1:]
+	if sig.endswith(")"):
+		sig = sig[:-1]
+	returns = split_balanced(sig)
+
+	return args, returns
+
+# takes gocode's candidate and returns sublime's hint and subj
+def hint_and_subj(cls, name, type):
+	subj = name
+	hint = subj + "\t" + cls
+	args, returns = extract_arguments_and_returns(type)
+	if returns:
+		hint = subj + "\t" + ", ".join(returns)
+	if not args and cls == "func":
+		subj += "()"
+	if args:
+		sargs = []
+		for i, a in enumerate(args):
+			ea = a.replace("{", "\\{").replace("}", "\\}")
+			sargs.append("${{{0}:{1}}}".format(i+1, ea))
+		subj += "(" + ", ".join(sargs) + ")"
+	return hint, subj
+
 class Gocode(sublime_plugin.EventListener):
 	def on_query_completions(self, view, prefix, locations):
 		loc = locations[0]
@@ -16,6 +102,7 @@ class Gocode(sublime_plugin.EventListener):
 		result = []
 		for line in filter(bool, out.split("\n")):
 			arg = line.split(",,")
-			result.append([arg[1] + "\t" + arg[0], arg[1]])
+			hint, subj = hint_and_subj(*arg)
+			result.append([hint, subj])
 
 		return (result, sublime.INHIBIT_WORD_COMPLETIONS)
