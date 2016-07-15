1be256566ef385eb611c90414f5163605d1bfb15
Anmol Sethi <anmol@aubble.com>
2016-07-15 04:13:20 -0400

    ensure gocode uses default settings with tests

--- _testing/all.bash
+++ _testing/all.bash
@@ -4,6 +4,7 @@ sleep 0.5
 echo "--------------------------------------------------------------------"
 echo "Autocompletion tests..."
 echo "--------------------------------------------------------------------"
+export XDG_CONFIG_HOME="$(mktemp -d)"
 ./run.rb
 sleep 0.5
 gocode close
