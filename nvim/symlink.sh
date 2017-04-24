8b988bbfbb445f0bec6a3ae56b5eeee75306b711
Lorenzo Manacorda <lorenzo@mailbox.org>
2017-04-24 18:56:08 +0200

    force symlink creation
    vim-plug's :PlugInstall! runs the callbacks each time.
    if the symlinks exist, the call failed.

--- nvim/symlink.sh
+++ nvim/symlink.sh
@@ -3,5 +3,5 @@ cd "${0%/*}"
 ROOTDIR=`pwd`
 mkdir -p "$HOME/.config/nvim/autoload"
 mkdir -p "$HOME/.config/nvim/ftplugin/go"
-ln -s "$ROOTDIR/autoload/gocomplete.vim" "$HOME/.config/nvim/autoload/"
-ln -s "$ROOTDIR/ftplugin/go/gocomplete.vim" "$HOME/.config/nvim/ftplugin/go/"
+ln -fs "$ROOTDIR/autoload/gocomplete.vim" "$HOME/.config/nvim/autoload/"
+ln -fs "$ROOTDIR/ftplugin/go/gocomplete.vim" "$HOME/.config/nvim/ftplugin/go/"
